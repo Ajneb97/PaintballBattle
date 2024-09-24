bf2907469c9f5d061fb3bbf79399d2bd17b8ebad
Ajneb97 <ajneb97@gmail.com>
2024-09-24 17:54:25 -0300

    1.11.2

--- src/pb/ajneb97/PaintballBattle.java
+++ src/pb/ajneb97/PaintballBattle.java
@@ -161,6 +161,7 @@ public class PaintballBattle extends JavaPlugin {
 				serverVersion = ServerVersion.v1_20_R4;
 				break;
 			case "1.21":
+			case "1.21.1":
 				serverVersion = ServerVersion.v1_21_R1;
 				break;
 			default:
